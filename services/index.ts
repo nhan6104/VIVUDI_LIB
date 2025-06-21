0b9ac9f3db4c8e8333adb54fe7ec59de50524513
nhan6104 <thanhan.nguyen6104@gmail.com>
2025-06-21 23:53:28 +0700

    update

--- services/index.ts
+++ services/index.ts
@@ -1,2 +1,2 @@
 // export const VIVUDI_API_URL = 'http://localhost:5001/api/v1';
-export const VIVUDI_API_URL = ' https://vivudi-backend.onrender.com/api/v1';
+export const VIVUDI_API_URL = 'https://vivudi-backend.onrender.com/api/v1';
